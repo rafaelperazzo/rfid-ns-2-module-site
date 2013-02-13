e7b6d9335f10c53d515f2e5216d42e62f7cd2143
Rafael Perazzo <perazzo@ime.usp.br>
2013-02-13 19:33:55 -0200

    Version 13/02/2013 19:33

--- www/ns-2/atualizar.sh
+++ www/ns-2/atualizar.sh
@@ -1,2 +1,4 @@
 #scp * perazzo@ime.usp.br:www
 rsync -Cravzpu --del --exclude 'tex' --exclude '*.no' --exclude '*.bak' --exclude '*.dia' /home/rafael/Dropbox/rafael/usp/ns-2 perazzo@ime.usp.br:www
+rm -f END
+echo $(date) >> END
